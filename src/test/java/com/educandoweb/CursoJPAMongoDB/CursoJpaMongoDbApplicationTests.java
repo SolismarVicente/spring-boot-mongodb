213194ea48a470826f9710be0bbb98b95d5312a7
solismarvicente <solismarvicente@gmail.com>
2021-03-12 17:59:12 -0300

    projeto criado

---
+++ src/test/java/com/educandoweb/CursoJPAMongoDB/CursoJpaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.CursoJPAMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
